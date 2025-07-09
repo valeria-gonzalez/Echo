52380765080eb89f0d674fc7cb0ab23acce801fe
Alan-codigo <100821721+Alan-codigo@users.noreply.github.com>
2025-07-09 01:12:02 -0600

    Update db.py

--- API/resourses_api/app/db.py
+++ API/resourses_api/app/db.py
@@ -3,6 +3,6 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from google.cloud.firestore_v1 import FieldFilter
 
-cred = credentials.Certificate('echo-9f9e9-firebase-adminsdk-fbsvc-eb86b9ff51.json')
+cred = credentials.Certificate('')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
