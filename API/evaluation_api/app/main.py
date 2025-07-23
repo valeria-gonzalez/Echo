0923de680006dc302fe779d4d9e732a41e80a588
valeria-gonzalez <valeriaacademique@gmail.com>
2025-07-23 17:30:22 -0600

    fix: add path to main
    Fix adding path so api can start.

--- API/evaluation_api/app/main.py
+++ API/evaluation_api/app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import sys
+import os
+
+# Add parent directory to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from routers.evaluation_router import router
 
 app = FastAPI(title="Pronunciation Evaluation API", version="1.0.0")
@@ -18,9 +24,9 @@ app.include_router(router)
 async def root():
     return {"message": "Welcome to the Pronunciation Evaluation API!"}
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     # If below doesn't work, run in terminal: uvicorn app.main:app --reload
-    #  import uvicorn
-    #  uvicorn.run("app.main:app", host="0.0.0.0", port=5001, reload=True)
+    import uvicorn
+    uvicorn.run("app.main:app", reload=True)
     
     
