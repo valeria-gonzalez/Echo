9e0eca4f237d9f2156a40db22ae8ad866b1b4976
Alan Josafat <alan2002050702@gmail.com>
2025-07-24 02:22:20 -0600

    feat: add import audio sentences

---
+++ data_import/import_audio_sentences.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+class ImportAudioSentences:
+
+    def __init__(self,api_direction_url_audio_sentences:str, 
+                 direction_audios_download:str):
+        self.api_direction_url_audio_sentences = api_direction_url_audio_sentences
+        self.direction_audios_download = direction_audios_download
+
+    def AudioImport(self):
+
+        audios_download =  self.direction_audios_download
+        list_audio = os.listdir(audios_download)   
+
+        for file_name in list_audio:
+            file_path = os.path.join(audios_download,file_name)
+            if os.path.isfile(file_path) and file_name.endswith('.mp3'):
+                with open(file_path, "rb") as f:
+                    upload_files = {"file": (file_name, f ,"audio/mpeg")}
+                    response = requests.post(self.api_direction_url_audio_sentences, files = upload_files )
+                    print(response)
--- data_import/test.py
+++ data_import/test.py
@@ -2,6 +2,8 @@ from import_words import ImportWords
 from import_texts import ImportTexts
 from import_sentences import ImportSentences
 
+from import_audio_sentences import ImportAudioSentences
+
 def main():
 
     #words
@@ -12,17 +14,26 @@ def main():
     api_direction_sentences = "http://127.0.0.1:8000/resourses/sentences"
     jsonl_direction_sentences = "../data_processing/dataset_manager/processed_datasets/sentences_processed.jsonl"
 
+    #sentences audio
+    api_direction_sentences_audio = "http://127.0.0.1:8000/resourses/sentences/audios"
+    download_audio = './data_download/audios'
+    
     #texts
     api_direction_texts = "http://127.0.0.1:8000/resourses/texts"
     jsonl_direction_texts = "../data_processing/dataset_manager/processed_datasets/texts_processed.jsonl"
+
 
     importWords = ImportWords(api_direction_words, jsonl_direction_words)
     importSentences = ImportSentences(api_direction_sentences, jsonl_direction_sentences)
     importTexts = ImportTexts(api_direction_texts,jsonl_direction_texts)
 
+    importAudioSentences = ImportAudioSentences(api_direction_sentences_audio, download_audio)
+    
+
     importWords.jsonlImport()
     importSentences.jsonImport()
     importTexts.jsonlImport()
+    importAudioSentences.AudioImport()  
 
 
 if __name__ == "__main__":
