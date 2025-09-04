e6350b2d672ef6bee9dd63ee77416108cf1d7a66
Alan Josafat <alan2002050702@gmail.com>
2025-09-03 20:01:57 -0600

    delete evaluation api and update .env and schemas

--- API/users_api/app/config.py
+++ API/users_api/app/config.py
@@ -8,4 +8,4 @@ load_dotenv()
 # Access variables
 FIREBASE_CREDENTIALS = os.getenv("FIREBASE_CREDENTIAL")
 if FIREBASE_CREDENTIALS is None:
-    raise ValueError("ARLI_API_KEY not found in environment variables.")
+    raise ValueError("FIREBASE_CREDENTIAL not found in environment variables.")
